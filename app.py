33bc32f4fac9c8545e063563f15b0580103aa21c
max <maxence.landa@gmail.com>
2024-12-06 01:51:56 +0100

    feat: update user

--- app.py
+++ app.py
@@ -11,29 +11,18 @@ def read_user(user_id: int):
     user = session.query(Users).where(Users.id == user_id).first()
     if user:
         return user
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f'User not found',
-    )
+    raise HTTPException(status_code=404, detail="User not found")
 
 @app.post("/user/create/")
 def create_user(user: PydanticUsers):
     user = session.query(Users).where(Users.username == user.username).first()
     if user:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail=f'Username already taken',
-        )
+        raise HTTPException(status_code=409, detail="Username taken")
     if len(user.password) < 5:
-        raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail=f'Password too short',
-        )
+        raise HTTPException(status_code=422, detail="Password too short")
     if len(user.username) < 4:
-        raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail=f'Username too short',
-        )
+        raise HTTPException(status_code=422, detail="Username too short")
+    
     new_user = Users(
         username = user.username,
         password = user.password,
@@ -41,7 +30,22 @@ def create_user(user: PydanticUsers):
         logo = user.logo,
         role = user.role
     )
+    
     session.add(new_user)
     session.commit()
-    print("created user")
     return user
+
+@app.put("/user/update/{user_id}")
+def update_user(user_id: int, user: Users):
+    db_user = session.query(Users).where(Users.id == user_id).first()
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    if user.iswordscloud:
+        db_user.iswordscloud = user.iswordscloud
+    if user.logo:
+        db_user.logo = user.logo
+    if user.role:
+        db_user.role = user.role
+
+    return db_user
