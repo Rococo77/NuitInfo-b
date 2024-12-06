66181e6339693c897620e2f805e8ba2895feaedf
max <maxence.landa@gmail.com>
2024-12-06 01:45:00 +0100

    feat: raise http exceptions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from models.users import session, Users
 from models.pydantic_users import PydanticUsers
 import sys
@@ -11,10 +11,29 @@ def read_user(user_id: int):
     user = session.query(Users).where(Users.id == user_id).first()
     if user:
         return user
-    return None
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f'User not found',
+    )
 
 @app.post("/user/create/")
 def create_user(user: PydanticUsers):
+    user = session.query(Users).where(Users.username == user.username).first()
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f'Username already taken',
+        )
+    if len(user.password) < 5:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f'Password too short',
+        )
+    if len(user.username) < 4:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f'Username too short',
+        )
     new_user = Users(
         username = user.username,
         password = user.password,
